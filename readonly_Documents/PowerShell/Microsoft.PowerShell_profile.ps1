cf40db2c80d2dce6fbd74e6fe77071bd2c2c2520
NydusBorn <nydusborn@outlook.com>
2025-04-18 20:15:02 +0300

    Update Documents/PowerShell/Microsoft.PowerShell_profile.ps1

--- readonly_Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ readonly_Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,3 +1,3 @@
-$ENV:STARSHIP_CONFIG = "%USERPROFILE%\Documents\PowerShell\starship.toml"
+$ENV:STARSHIP_CONFIG = "$ENV:USERPROFILE\Documents\PowerShell\starship.toml"
 Invoke-Expression (&starship init powershell)
 Invoke-Expression ("fastfetch")
